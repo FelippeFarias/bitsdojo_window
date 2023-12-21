c1585ee0f877c37bae0d911ddf64151295d1a18d
Felippe Farias <adm@ffsdevelopers.com>
2023-12-21 16:22:36 -0300

    update win32 lib for 5.1.1

--- bitsdojo_window_linux/README.md
+++ bitsdojo_window_linux/README.md
@@ -1,5 +1,5 @@
-# bitsdojo_window_linux
+# bitsdojo_window_linux_v3
 
 The Linux implementation of [`bitsdojo_window`][1].
 
-[1]: https://pub.dev/packages/bitsdojo_window
+[1]: https://pub.dev/packages/bitsdojo_window_v3
--- bitsdojo_window_linux/linux/flutter/generated_plugin_registrant.cc
+++ bitsdojo_window_linux/linux/flutter/generated_plugin_registrant.cc
@@ -4,7 +4,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <bitsdojo_window_linux/bitsdojo_window_plugin.h>
+#include <bitsdojo_window_linux_v3/bitsdojo_window_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) bitsdojo_window_linux_registrar =
